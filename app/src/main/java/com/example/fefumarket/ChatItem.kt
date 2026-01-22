558efa8de39104dafe90d7283ebec2c0917ef603
bgangval <91944658+bgangval@users.noreply.github.com>
2026-01-22 16:59:31 +1000

    Delete app directory

--- app/src/main/java/com/example/fefumarket/ChatItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fefumarket
-
-data class ChatItem(
-    val sellerName: String,      // Имя продавца
-    val productName: String,     // Название товара
-    var lastMessage: String,     // Последнее сообщение
-    val avatarResId: Int,         // Фото товара
-    var isMuted: Boolean = false  //mute
-)
